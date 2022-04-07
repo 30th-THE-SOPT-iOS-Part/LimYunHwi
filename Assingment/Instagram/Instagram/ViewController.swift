d7330a619533e667da2842a6b7fbf3aaa634a427
LimYunhwi <88yhtserof@naver.com>
2022-04-07 14:45:36 +0900

    #1 [Feat] iOS 1차 과제  : 데이터 전달(2)
    1. 로그인 시 완료 화면으로 사용자 데이터 전달
    2. 사용자 ID, 비밀번호 모두 작성 시 '로그인' 버튼 활성화

--- Assingment/Instagram/Instagram/ViewController.swift
+++ Assingment/Instagram/Instagram/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var userIdTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var user: User?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,9 +21,24 @@ class ViewController: UIViewController {
     }
 
     func configureView(){
-        loginButton.isUserInteractionEnabled = false
-        loginButton.alpha = 0.5
+        self.loginButton.isUserInteractionEnabled = false
+        self.loginButton.alpha = 0.5
     }
-
+    
+    @IBAction func editTextField(_ sender: UITextField) {
+        self.loginButton.isUserInteractionEnabled = true
+        self.loginButton.alpha = 1
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "CompleteSignUpViewController" {
+            guard let completeSignUpVC = segue.destination as? CompleteSignUpViewController else {return}
+            guard let userId = self.userIdTextField.text else {return}
+            guard let password = self.passwordTextField.text else {return}
+            user = User(userName: userId, password: password)
+            
+            completeSignUpVC.user = user
+        }
+    }
 }
 
