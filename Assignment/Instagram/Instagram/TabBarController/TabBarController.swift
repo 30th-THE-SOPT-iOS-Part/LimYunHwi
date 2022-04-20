efbadd78195b6842e18598a2c24b9405756d189b
LimYunhwi <88yhtserof@naver.com>
2022-04-20 16:12:51 +0900

    #3 [Feat] iOS 2차 과제  : Reels 화면 UI 수정
    1. Reels TabBar Item 선택 시 다크모드로 전환

--- Assignment/Instagram/Instagram/TabBarController/TabBarController.swift
+++ Assignment/Instagram/Instagram/TabBarController/TabBarController.swift
@@ -8,8 +8,25 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    var modeStyle: UIUserInterfaceStyle = .light {
+        didSet{
+            self.overrideUserInterfaceStyle = modeStyle
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.delegate = self
+    }
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        if let reelsViewController = viewController as? ReelsViewController {
+            modeStyle = .dark
+        } else {
+            modeStyle = .light
+        }
     }
 }
