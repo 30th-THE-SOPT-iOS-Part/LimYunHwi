69cf51e0ec7454a2ac58f806075c1d620c4aa878
LimYunHwi <88yhtserof@naver.com>
2022-05-11 21:31:37 +0900

    [Feat] iOS 4차 과제 : 회원가입 API 연결 (1) #7
    1. HTTP 연결 허용
    2. 네트워크 URL  모델 생성
    3. 네트워크 결과, 네트워크 디코딩 모델 생성
    4. 회원가입 API 연결을 위한 Service 생성

---
+++ Assignment/Instagram/Instagram/Network/APIConstants.swift
@@ -0,0 +1,17 @@
+//
+//  APIConstants.swift
+//  Instagram
+//
+//  Created by 임윤휘 on 2022/05/11.
+//
+
+import Foundation
+
+struct APIConstants {
+    //MARK: - Base URL
+    static let baseURL = "http://13.124.62.236"
+    
+    //MARK: - Feature URL
+    //signInURL = "http://13.124.62.236/auth/signup"
+    static let signUpURL = baseURL + "/auth/signup"
+}
